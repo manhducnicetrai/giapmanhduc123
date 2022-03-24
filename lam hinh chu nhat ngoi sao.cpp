37f434a39e8f9847cb99fb3feb366b9751f8d481
manhducnicetrai <giapmanhduc4real@gmail.com>
2022-03-24 21:07:49 +0700

    Push_file_lam hinh chu nhat ngoi sao.cpp

---
+++ lam hinh chu nhat ngoi sao.cpp
@@ -0,0 +1,16 @@
+#include <iostream.h>
+int main(int argc, char *argv[])
+{
+	int i,j;
+	for(i=1;i<=4;i++)
+	{    
+		for(j=1;j<=9;j++){
+			
+		
+		    cout<<"*";}
+		    
+		 cout<<"*"<<endl;
+	}
+	
+	return 0;
+}
